981971e1a1bcc5a56826a6092c82a33ba017010c
Andrew <andros818@gmail.com>
2015-12-02 14:13:07 -0500

    new gitignore; new project file

---
+++ TheHerd-Arcade/Assets/Scripts/Controller.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+	public float speed;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+		
+	}
+
+	void Controls(){
+		if (input.GetKey(KeyCode.ArrowUp)){
+			//transform.position = 
+		}
+		if (input.GetKey(KeyCode.ArrowDown)){
+
+		}
+		if (input.GetKey(KeyCode.ArrowLeft)){
+
+		}
+		if (input.GetKey(KeyCode.ArrowRight)){
+
+		}
+	}
+
+}
