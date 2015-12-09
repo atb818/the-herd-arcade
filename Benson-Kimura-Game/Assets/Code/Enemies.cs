7216f6001dac9244c2a2200cdd01e5e684f00d10
Andrew <andros818@gmail.com>
2015-12-09 14:15:42 -0500

    enemies move towards player

---
+++ Benson-Kimura-Game/Assets/Code/Enemies.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemies : MonoBehaviour {
+
+	public Transform target;
+	//RigidBody2D rb;
+
+	public float speed;
+	//float range;
+
+	void Start () {
+		//rb = GetComponent<RigidBody2D>();
+	}
+	
+	void FixedUpdate () {
+		Move();
+	}
+
+	void Move(){
+		//rb.AddForce()
+		//range = Vector2.Distance(transform.position, target.position);
+		//transform.Translate(Vector2.MoveTowards(transform.position, target.position, range) * speed * Time.deltaTime);
+		transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+	}
+}
---
+++ Benson-Kimura-Game/Assets/Code/Weapon.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void OnTriggerEnter2D (Collider2D enemy) {
+		if (enemy.CompareTag("Enemy")){
+			Destroy(enemy.gameObject);
+		}
+	}
+}
